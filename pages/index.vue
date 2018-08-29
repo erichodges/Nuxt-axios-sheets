85a573b3021c5a112d8bcc9f54b6bd91ac72a00f
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2018-08-29 14:36:20 -0700

    display of data fixed

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,18 @@
 <template>
-  <div>
-    <md-table v-model="test" md-sort="last-name" md-sort-order="asc" md-card>
+    <div>
+
+    <md-table v-model="teachers" md-sort="lastName" md-sort-order="desc" md-card>
       <md-table-toolbar>
         <h1 class="md-title">Marin Music Teachers</h1>
       </md-table-toolbar>
 
       <md-table-row slot="md-table-row" slot-scope="{ item }">
-        <md-table-cell md-label="First Name" md-sort-by="first-name">{{ item.first-name }}</md-table-cell>
-        <md-table-cell md-label="Last Name" md-sort-by="last-name">{{ item.last-name }}</md-table-cell>
+        <md-table-cell md-label="First Name" md-sort-by="firstName">
+          {{ item.firstName }}
+        </md-table-cell>
+        <md-table-cell md-label="Last Name" md-sort-by="lastName">
+          {{ item.lastName }}
+        </md-table-cell>
         <md-table-cell md-label="Travel?" md-sort-by="travel">{{ item.travel }}</md-table-cell>
         <md-table-cell md-label="Address" md-sort-by="address">{{ item.address }}</md-table-cell>
         <md-table-cell md-label="City" md-sort-by="city">{{ item.city }}</md-table-cell>
@@ -17,10 +22,6 @@
       </md-table-row>
     </md-table>
 
-    <!-- <div id="cards" v-for="currency in currencies" :key="currency[0]">
-      <card :name="currency[0]" :price_usd="currency.price_usd" :percent_change_1h="currency.percent_change_1h"></card>
-    </div> -->
-    
   </div>
 </template>
 
@@ -29,35 +30,41 @@ import axios from "axios";
 require('dotenv').config()
 import Card from "~/components/Card.vue";
 
-const convert = function(data) {
-  let keys = data[0];
-  let values = data.slice(1);
-  let object = {};
-  let objects = values.map(array => {
-    object = {};
-
-    keys.forEach((key, i) => object[key] = array[i]);
-  
-    return object;
-  });
-  this.test = JSON.stringify(objects);
-// console.log(JSON.stringify(objects));
-}
-
-
 export default {
-  asyncData() {
-    return axios
-      .get(`https://sheets.googleapis.com/v4/spreadsheets/1SIfFSp_1In8V_NmIGjVtry-7478OJosu91J_toQT7gs/values/Sheet1?valueRenderOption=FORMATTED_VALUE&key=${process.env.API_KEY}`)
-      .then(response => {
-        return convert(response.data.values);          
-      });     
-  },
-  data: () => {
-    test: []
+  data() {
+    return {
+      teachers: []
+    }
+
   },
   components: {
     Card
+  },
+  mounted() {
+    this.getData()
+  },
+  methods: {
+    onConvert(data) {
+      let keys = data[0];
+      let values = data.slice(1);
+      let object = {};
+      let objects = values.map(array => {
+        object = {};
+
+        keys.forEach((key, i) => object[key] = array[i]);
+
+        return object;
+      });
+      this.teachers = objects;
+    },
+
+    getData() {
+      return axios
+        .get(`https://sheets.googleapis.com/v4/spreadsheets/1SIfFSp_1In8V_NmIGjVtry-7478OJosu91J_toQT7gs/values/Sheet1?valueRenderOption=FORMATTED_VALUE&key=${process.env.API_KEY}`)
+        .then(response => {
+          this.onConvert(response.data.values);
+        });
+    },
   }
 };
 </script>
